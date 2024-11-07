6719106bb178ea44bb94859bfc2f1f6b3298cf3b
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-11-07 08:52:47 +0700

    edit message for email forgot password

--- src/libs/nodemailer.ts
+++ src/libs/nodemailer.ts
@@ -1,7 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.APP_PASSWORD
@@ -12,15 +14,18 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
     const resetLink = `http://localhost:5173/reset-password/${token}`;
     
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: `"Haloa App" <${process.env.EMAIL_USER}>`,
         to: email,
         subject: 'Reset Password Link',
         html: `
-            <h1>Reset Password</h1>
-            <p>Please click the link below to reset your password:</p>
-            <a href="${resetLink}">${resetLink}</a>
+            <h2>Reset Password</h2>
+            <p>Hi ${email},</p>
+            <p>Please click the link below to reset your password, and please don't share this link to anyone.</p>
+            <button><a href="${resetLink}" target="_blank" style="text-decoration: none; color: white; background-color: #007bff; hover: background-color: #0056b3; font-weight: bold; padding: 10px 20px; border-radius: 5px;">Reset Password</a></button>
             <p>This link will expire in 1 hour.</p>
-            <p>If you did not request a password reset, please ignore this email.</p>
+            <p>This message is automatically sent by the system, if you did not request a password reset, please ignore this email.</p>
+            <p>Regards,</p>
+            <p>Haloa App Team</p>
         `
     };
 
