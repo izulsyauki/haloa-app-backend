9f6d8cd9e00582939c2b92f8d012c432d4afd8a6
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-11-10 15:45:25 +0700

    setting upstash redis

---
+++ src/libs/upstash-redis.ts
@@ -0,0 +1,15 @@
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+(async () => {
+  try {
+    const data = await redis.get("key");
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+})();
