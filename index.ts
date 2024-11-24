6932fb79d772054f263b42b11d9ba131c2eb22f9
Izulsyauki Imani <izulsyaukiimani@gmail.com>
2024-11-25 05:46:03 +0700

    setting cors origin

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import express, { Express, Response, Request, urlencoded } from "express";
 import router from "./src/routes";
 import dotenv from "dotenv";
-import cors from "cors";
+import Cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 
 const app: Express = express();
-app.use(cors());
+app.use(Cors({
+    origin: [process.env.CORS_ORIGIN_DEV || "", process.env.CORS_ORIGIN_PROD || ""],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
